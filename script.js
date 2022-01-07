7b70175fff66c90c790fc6dddffcaa060f3e06d5
Herus Macedo <herus2019@hotmail.com>
2022-01-07 11:19:18 -0300

    Funcionalidade de Remover tarefa adicionada

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-document.querySelector('#add').onclick = function() {
+document.querySelector('#add').onclick = function () {
 
-if(document.querySelector('#novaTarefa input').value == 0){
+    if (document.querySelector('#novaTarefa input').value == 0) {
 
-    swal("Insira uma tarefa!", "Você precisa inserir uma tarefa no campo!", "error");
+        swal("Insira uma tarefa!", "Você precisa inserir uma tarefa no campo!", "error");
 
-}
+    } else {
+
+        document.querySelector('#tarefas').innerHTML +=
+
+            `
+
+        <div class="tarefa">
+
+        <span id="nomedaTarefa">
+            
+            ${document.querySelector('#novaTarefa input').value}
+        
+        </span>
+
+        <button class="deletar"> 
+        
+        <i class="far fa-trash-alt"></i>
+        
+        </button>
+
+        </div>
+        `;
+
+
+        var tarefa_a_ser_feita = document.querySelectorAll(".deletar"); {
+
+            for (var i = 0; i < tarefa_a_ser_feita.length; i++) {
+
+                tarefa_a_ser_feita[i].onclick = function () {
+
+                    this.parentNode.remove();
+
+                }
+
+            }
+
+
+        }
+
+
+
+    }
 
 }
